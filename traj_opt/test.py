376ecb0cbec241a273e21be5f0d45a43ecfb9a69
Jordan Ford <jsford94@gmail.com>
2017-03-29 17:32:37 -0400

    Still playing around. I believe my forward dynamics are correct. Now experimenting with boundary conditions and cost functions. Soon I will try plugging it into whatever the python equivalent of fmincon is.

--- traj_opt/test.py
+++ traj_opt/test.py
@@ -2,27 +2,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pdb
 
-epsilon = 1e-5
+epsilon = 1e-3
 
-def forward_dynamics( p, s ):
+# This function describes state as a function of the 
+# parameter vector p and the arclength s
+def calc_f( p, s ):
     s = np.arange(0, s, epsilon)
     u = p[0] + s*(p[1] + s*(p[2] + s*(p[3])))
-    k = u
-    theta = np.cumsum(k)*epsilon
-    x = np.cumsum(np.cos(theta))*epsilon
-    y = np.cumsum(np.sin(theta))*epsilon
 
-    plt.plot(x, y, 'red')
-    plt.show()
+    k = u 
+    theta = np.cumsum(k)*epsilon 
+    x = np.cumsum(np.cos(theta))*epsilon 
+    y = np.cumsum(np.sin(theta))*epsilon 
+    plt.plot(x, y)
 
     return np.array([x[-1],y[-1],theta[-1],k[-1]])
 
-def boundary_conditions_h(p, s):
-    return forward_dynamics(p, 0) + forward_dynamics(p, s)
-     
-    
-def boundary_conditions_g(p, s, xb):
-    return boundary_conditions_h(p, s) - xb
-    
+# This function describes the change in state as a function of the 
+# parameter vector p and the arclength s
+def calc_f_dot( p, s ):
+    s = np.arange(0, s, epsilon)
+    u = p[0] + s*(p[1] + s*(p[2] + s*(p[3])))
 
-print forward_dynamics( np.array([0,33,-82,41.5]), 1.1)
+    k = s*(p[1] + 2*s*(p[2] + 3*s*(p[3])))
+    theta = u 
+    x = np.cos(theta) 
+    y = np.sin(theta) 
+
+    return np.array([x[-1],y[-1],theta[-1],k[-1]])
+
+# The boundary condition is that the final posture should be the same as xb, the
+# as xb, the boundary posture
+def boundary_conditions_g(q, xb):
+    return calc_f(q[0], q[1]) - xb
+
+
+def cost_function_J(q, xd):
+    sum = 0
+    for s in np.arange(epsilon, q[1], 1000*epsilon):
+        xf = calc_f(q[0], s)
+        sum += xf[0] + xf[1] + xf[2]*1000*epsilon + xf[3]*1000*1000*epsilon*epsilon
+    return sum/2.0 
+
+p = [0, 33, -82, 41.5]
+xb = [0.70102248, 0.52060821, -1.22559075, -7.68353244]; 
+
+
+p1 = np.arange(32, 34, .1)
+g = []
+J = []
+
+for new_p in p1:
+    p[1] = new_p 
+    g.append(boundary_conditions_g([p, 1.1], xb))
+    J.append(cost_function_J([p, 1.1], xb))
+
+plt.show()
+plt.plot(p1,g)
+plt.show()
+
+
+
+
+
+
+
+
+
+
