cce38bf6e7edf0f1cd4057e6471e2dbf7ff11f4a
Jordan Ford <jsford94@gmail.com>
2017-03-31 20:54:20 -0400

    Tried to enforce a distinction between row and column vectors. PITA. Getting a different, seemingly worse, answer now.

--- traj_opt/test.py
+++ traj_opt/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-import numpy as np
 import matplotlib.pyplot as plt
 import pdb
+from numpy import *
 from scipy.optimize import fsolve
 
 epsilon = 1e-3
@@ -10,45 +10,49 @@ POLY_DEG = 4
 
 # This function describes state as a function of the 
 # parameter vector p and the arclength s
+# Inputs:          q    dim: (1, POLY_DEG+1)
+# Output:   [x,y,t,k]   dim: (1, 4)
 def calc_f(q, plot=False, color='black'):
-    s = np.arange(0, q[0], epsilon)
-    u = q[1] + s*(q[2] + s*(q[3] + s*(q[4])))
+    s = arange(0, q[0,0], epsilon)
+    s = s[:, newaxis]
+    u = q[0,1] + s*(q[0,2] + s*(q[0,3] + s*(q[0,4])))
 
     k = u 
-    theta = np.cumsum(k)*epsilon 
-    x = np.cumsum(np.cos(theta))*epsilon 
-    y = np.cumsum(np.sin(theta))*epsilon 
+    theta = cumsum(k)*epsilon 
+    x = cumsum(cos(theta))*epsilon 
+    y = cumsum(sin(theta))*epsilon 
     if(plot):
         plt.plot(x, y, color)
 
-    return np.array([x[-1],y[-1],theta[-1],k[-1]])
+    return matrix([x[-1],y[-1],theta[-1],k[-1]])
 
 def boundary_function_g(q, x0, xd):
     h = calc_f(q) + x0
     g = h-xd
-    return g
+    return g.reshape((1,POLY_DEG))
 
 # Change this to output a matrix 
 # with (x,y,k,t)' on the side and (sf, p1, p2, p3, p4) on the top
 def g_dq(q, x0, xd):
-    grad = np.zeros((len(q), 4))
+    grad = zeros((len(q), 4))
     for p_idx in range(0, len(q)):
         perturbed = q.copy()
         q[p_idx] += epsilon
-        grad[p_idx,:] = np.transpose(boundary_function_g(q, x0, xd)
+        tmp = transpose(boundary_function_g(q, x0, xd)
                          - boundary_function_g(perturbed, x0, xd))
-    np.divide(grad, epsilon)
-    return np.transpose(grad)
+        grad[p_idx,:] = tmp.reshape(4,)
+    divide(grad, epsilon)
+    return transpose(grad)
 
 def cost_function_J(q):
-    s = np.arange(0, q[0], epsilon)
-    k = q[1] + s*(q[2] + s*(q[3] + s*q[4]))
+    s = arange(0, q[0,0], epsilon)
+    k = q[0,1] + s*(q[0,2] + s*(q[0,3] + s*q[0,4]))
 
-    sum_squares = 0.5*np.sum(k*k)
+    sum_squares = 0.5*sum(k*k)
     return sum_squares 
 
 def J_dq(q):
-    grad = np.zeros((1, len(q)))
+    grad = zeros((1, len(q)))
     for p_idx in range(0, len(q)):
         perturbed = q.copy()
         perturbed[p_idx] += epsilon
@@ -57,13 +61,13 @@ def J_dq(q):
 
 
 def calc_L(params, x0, xd):
-    q = params[0:POLY_DEG+1]
-    l_mult = params[POLY_DEG+1:] 
-    return cost_function_J(q) + np.dot(l_mult, boundary_function_g(q, x0, xd))
+    q = params[0:POLY_DEG+1].reshape(1, POLY_DEG+1)
+    l_mult = params[POLY_DEG+1:].reshape(4,1)
+    return cost_function_J(q) +  boundary_function_g(q, x0, xd) * l_mult
 
 
 def grad_L(params, x0, xd):
-    grad = np.zeros((1, len(params)))
+    grad = zeros((1, len(params)))
     
     for p_idx in range(0, len(params)):
         center = calc_L(params, x0, xd)
@@ -75,23 +79,27 @@ def grad_L(params, x0, xd):
 
 
 def init_sf(x0, xf):
-    rad = np.sqrt((xf[0]-x0[0])**2 + (xf[1]-x0[1])**2)
-    sf = rad * ( ( xf[2]**2 )/5 + 1 )
+    rad = sqrt((xf[0,0]-x0[0,0])**2 + (xf[0,1]-x0[0,1])**2)
+    sf = rad * ( ( xf[0,2]**2 )/5 + 1 )
     return sf
 
 # Could be improved to make a 3rd order guess by iteratively zeroing 
 # one parameter and solving for the others. For now, it just sets all params
 # but the first two to zero
+# Input:    sf      scalar
+#           x0      matrix  (1, 4)
+#           xf      matrix  (1, 4)
+# Output:   init_p  matrix  (1, POLY_DEG)
 def init_p(sf, x0, xf):
-    t0 = x0[2]; tf = xf[2];
-    k0 = x0[3]; kf = xf[3];
+    t0 = x0[0,2]; tf = xf[0,2];
+    k0 = x0[0,3]; kf = xf[0,3];
 
     dt = tf-t0
     dk = kf-k0
 
-    init_p = np.zeros((POLY_DEG, ))
-    init_p[0] = 2*dt/sf - dk
-    init_p[1] = (dk - init_p[0])/sf
+    init_p = zeros((1, POLY_DEG))
+    init_p[0,0] = 2*dt/sf - dk
+    init_p[0,1] = (dk - init_p[0,0])/sf
             
     return init_p
 
@@ -102,26 +110,27 @@ def init_lambda(q, x0, xf):
     dg_dq = g_dq(q, x0, xf) 
     dJ_dq = J_dq(q)
 
-    dg_dq_T_pinv = np.linalg.pinv(np.transpose(dg_dq))
-    lambduh = np.dot( dg_dq_T_pinv , np.transpose(-dJ_dq) )
-    return lambduh[:,0]
+    dg_dq_T_pinv = linalg.pinv(transpose(dg_dq))
+    lambduh = dg_dq_T_pinv * transpose(-dJ_dq)
+    return lambduh.reshape(1,4)
     
 
 # sf, p
-q = [1.1, 0, 33, -82, 41.5]
+q = matrix([1.1, 0, 33, -82, 41.5])
 
-x0 = [0, 0, 0, 0]
-xd = [0.70102248, 0.52060821, -1.22559075, -7.68353244]; 
+x0 = zeros((1, POLY_DEG)) 
+xd = matrix([0.70102248, 0.52060821, -1.22559075, -7.68353244]);
 
 
 sf_guess = init_sf(x0, xd)
 p_guess = init_p(sf_guess, x0, xd)
-q_guess = np.insert(p_guess, 0, np.array(sf_guess))
+q_guess = insert(p_guess, 0, sf_guess)
+q_guess = q_guess[newaxis, :]           # Enforce row vector
 
 lambda_guess = init_lambda(q_guess, x0, xd)
-param_guess = np.concatenate((q_guess, lambda_guess))
+param_guess = concatenate((q_guess, lambda_guess), axis=1)
 
-q_opt = fsolve(grad_L, param_guess, (x0, xd))
+q_opt = fsolve(grad_L, param_guess, (x0, xd)).reshape(1, POLY_DEG+1+4)
 
 
 calc_f(q_guess, plot=True, color='red') 
